6b573bf74199206fe0cc2b5e069f85228998a543
orshemesh <orshemesh@mail.tau.ac.il>
2022-05-17 16:06:36 +0300

    finished part 1 - before testing

--- MavnatProject2/OAHashTable.java
+++ MavnatProject2/OAHashTable.java
@@ -27,7 +27,11 @@ public abstract class OAHashTable implements IHashTable {
 	public void Insert(HashTableElement hte) throws TableIsFullException,KeyAlreadyExistsException {
 		for (int i=0; i<table.length; i++) {
 			int j = Hash(hte.GetKey(), i);
-			if (table[j] == null) {
+			if (table[j] != null) {
+				if (table[j].GetKey()==-1) {
+					table[j] = hte;
+					return;
+				}
 				if (table[j].GetKey() == hte.GetKey())
 					throw new KeyAlreadyExistsException(hte);
 			}
@@ -43,8 +47,13 @@ public abstract class OAHashTable implements IHashTable {
 	public void Delete(long key) throws KeyDoesntExistException {
 		for (int i=0; i<table.length; i++){
 			int j = Hash(key, i);
-			if (table[j].GetKey()==key)
-				table[j] = new HashTableElement(-1,-1);
+			if (table[j]==null)
+				throw new KeyDoesntExistException(key);
+			else {
+				if (table[j].GetKey() == key)
+					table[j] = new HashTableElement(-1, -1);
+					return;
+			}
 		}
 		throw new KeyDoesntExistException(key);
 	}
