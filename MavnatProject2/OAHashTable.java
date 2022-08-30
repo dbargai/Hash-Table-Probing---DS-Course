33738c4f22673d3364172e8909971cfadc29fcf7
dbargai <91421876+dbargai@users.noreply.github.com>
2022-08-30 15:23:18 +0300

    Update OAHashTable.java

--- MavnatProject2/OAHashTable.java
+++ MavnatProject2/OAHashTable.java
@@ -1,12 +1,3 @@
-//Student 1:
-//name: Dvir Bargai
-//ID: 208772715
-//username: dvirbargai
-
-//Student 2:
-//name: Or Shemesh
-//ID: 208992883
-//username: orshemesh
 
 public abstract class OAHashTable implements IHashTable {
 	
